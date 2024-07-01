9ed6403845505f2de2749aef25804253f2442077
AndreyWarGold-coder <alaxbabax12g@gmail.com>
2024-07-01 21:30:56 +0300

    add accs

--- services/AccSessionList.py
+++ services/AccSessionList.py
@@ -8,18 +8,18 @@ class AccSessionList:
         self.ses_list = {} #{"name_session": {"session": obj; "count": int}}
         self.accs_controller = AccsController()
 
-    async def start_session(self, name: str):
-        tmp = self.accs_controller.get_by(name=name, is_active=True)
+    async def start_session(self, phone: str):
+        tmp = self.accs_controller.get_by(phone=phone, is_active=True)
         if len(tmp) == 0:
             return False
         acc = tmp[0]
         if acc.proxy_id == None:
-            self.ses_list[name] = {"session": TelegramClient(session=acc.session_name,
+            self.ses_list[phone] = {"session": TelegramClient(session=acc.session_name,
                                                              api_id=acc.api_id,
                                                              api_hash=acc.api_hash),
                                    "count": 0}
         else:
-            self.ses_list[name] = {"session": TelegramClient(session=acc.session_name,
+            self.ses_list[phone] = {"session": TelegramClient(session=acc.session_name,
                                                              api_id=acc.api_id,
                                                              api_hash=acc.api_hash,
                                                              proxy=(
@@ -31,35 +31,38 @@ class AccSessionList:
                                                                  acc.proxy.password
                                                              )),
                                    "count": 0}
-        await self.ses_list[name]["session"].connect()
+        await self.ses_list[phone]["session"].connect()
         return True
 
-    async def end_session(self, name:str):
-        acc = self.ses_list.get(name, None)
+    async def end_session(self, phone:str):
+        acc = self.ses_list.get(phone, None)
         if acc == None:
             return False
         await acc["session"].disconnect()
-        self.ses_list.pop(name)
+        self.ses_list.pop(phone)
         return True
 
-    async def get_session(self, name:str):
-        acc = self.ses_list.get(name, None)
+    async def get_session(self, phone:str):
+        acc = self.ses_list.get(phone, None)
         if acc == None:
-            res = await self.start_session(name)
+            res = await self.start_session(phone)
             if res:
-                self.ses_list[name]["count"] += 1
-                return self.ses_list[name]["session"]
+                self.ses_list[phone]["count"] += 1
+                return self.ses_list[phone]["session"]
             else:
                 raise Exception("Session dont start")
         else:
-            self.ses_list[name]["count"] += 1
-            return self.ses_list[name]["session"]
+            self.ses_list[phone]["count"] += 1
+            return self.ses_list[phone]["session"]
 
-    async def take_session(self, name:str):
-        acc = self.ses_list.get(name, None)
+    async def give_away_session(self, phone:str):
+        acc = self.ses_list.get(phone, None)
         if acc == None:
             return False
-        self.ses_list[name]["count"] -= 1
-        if self.ses_list[name]["count"] <= 0:
-            await self.end_session(name)
+        self.ses_list[phone]["count"] -= 1
+        if self.ses_list[phone]["count"] <= 0:
+            await self.end_session(phone)
         return True
+
+
+session_list = AccSessionList()
